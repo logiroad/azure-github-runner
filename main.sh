1d663f72e98d2a603edd02ec3fe8c34b852c90a8
Maxime Zaremba <Maxz44@users.noreply.github.com>
2023-07-10 14:56:02 +0200

    Use env var with default value

--- main.sh
+++ main.sh
@@ -6,20 +6,24 @@ type az gh > /dev/null
 template_install() {
     template_file="install.sh"
     sed_script="s|{{token}}|${TOKEN}|g"
-    sed_script="${sed_script};s|{{repo}}|${REPO}|g"
+    sed_script="${sed_script};s|{{repo}}|${GITHUB_REPO}|g"
     sed_script="${sed_script};s|{{label}}|${UNIQ_LABEL}|g"
     sed "${sed_script}" "${template_file}.template" > "${template_file}"
 }
 
-RESOURCE_GROUP_NAME=testazcli
-LOCATION=northeurope
+: "${RESOURCE_GROUP_NAME:='testazcli'}"
+: "${LOCATION:='northeurope'}"
 VM_NAME="${RESOURCE_GROUP_NAME}vm"
-VM_IMAGE="Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest"
-REPO="user/repo"
-VM_USERNAME=vm
+: "${VM_IMAGE:='Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest'}"
+VM_USERNAME='vm'
+
+if [[ -z $GITHUB_REPO ]];then
+    >&2 echo "env var GITHUB_REPO not defined" 
+    exit 1
+fi
 
 UNIQ_LABEL=$(cat /dev/urandom | tr -cd '[:alpha:]' | head -c 6)
-TOKEN=$(gh api -XPOST --jq '.token' "repos/${REPO}/actions/runners/registration-token")
+TOKEN=$(gh api -XPOST --jq '.token' "repos/${GITHUB_REPO}/actions/runners/registration-token")
 
 if [[ $1 = '--destroy' ]]; then
     echo "Unregister runner"
